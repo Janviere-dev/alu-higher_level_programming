6d01e7cb3791451021d29ac644de78a6adae0f19
Janviere-dev <j.munezero@alustudent.com>
2025-02-18 05:02:41 -0800

    print the last digit of the number stored in the variable number.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number  >=10:
-     last digit = number % 10
- else:
-     lsat digit = (((number * -1) % 10) * -1)
-     string = "last digit of"
-     if last digit >5:
-         print(f"{string} {number} is {last digit} and is greater than 5")
-     elif last digiy ==0:
-         print(f"{string} {number} is {last digit} and is 0")
-     else:
-         print(f"{string} {number} is {last digit} and is less than 6 and not 0")
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = (((number * -1) % 10) * -1)
+string = "Last digit of"
+if lastDigit > 5:
+    print(f"{string} {number} is {lastDigit} and is greater than 5")
+elif (lastDigit < 6) and (lastDigit != 0):
+    print(f"{string} {number} is {lastDigit} and is less than 6 and not 0")
+else:
+    print(f"{string} {number} is {lastDigit} and is 0")
