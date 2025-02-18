37c3edde94624be1055673f0e2744615ae88ec34
Janviere-dev <j.munezero@alustudent.com>
2025-02-18 08:33:39 -0800

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-if number >= 0:
-    lastDigit = number % 10
-else:
-    lastDigit = (((number * -1) % 10) * -1)
-string = "Last digit of"
-if lastDigit > 5:
-    print(f"{string} {number} is {lastDigit} and is greater than 5")
-elif (lastDigit < 6) and (lastDigit != 0):
-    print(f"{string} {number} is {lastDigit} and is less than 6 and not 0")
-else:
-    print(f"{string} {number} is {lastDigit} and is 0")
+if __name__ == "__main__":
+    """
+        Imports functions from a module, does some Maths.
+    """
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
